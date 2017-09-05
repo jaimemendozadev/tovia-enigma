a9c6b5c638b9205d6ee933620ab091a0897a6856
jaimemendozadev <jaimemendozadev@gmail.com>
2017-09-05 13:07:41 -0700

    Start fix errors in eslint console

--- dev/components/form.jsx
+++ dev/components/form.jsx
@@ -3,10 +3,11 @@ import Avatar from 'react-toolbox/lib/avatar';
 import Input from 'react-toolbox/lib/input';
 import DatePicker from 'react-toolbox/lib/date_picker';
 import { Button } from 'react-toolbox/lib/button';
+import PropTypes from 'prop-types';
 
 const Form = props => (
   <form>
-    <h1>Tovia's Enigma</h1>
+    <h1>{"Tovia's Enigma"}</h1>
     <div className="avatar-container">
       <Avatar className="avatar">
         <img alt="" src="ic_account_circle_white_48dp_1x.png" />
@@ -46,5 +47,17 @@ const Form = props => (
     </div>
   </form>
 );
+
+Form.propTypes = {
+  sender: PropTypes.string.isRequired,
+  handleSender: PropTypes.func.isRequired,
+  unencrypted: PropTypes.string.isRequired,
+  handleMessage: PropTypes.func.isRequired,
+  handleDate: PropTypes.func.isRequired,
+  date: PropTypes.object.isRequired,
+  handleEncrypt: PropTypes.func.isRequired,
+  decryptMsg: PropTypes.func.isRequired,
+};
+
 
 export default Form;
--- dev/index.jsx
+++ dev/index.jsx
@@ -1,17 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {Snackbar} from 'react-toolbox'
+import { Snackbar } from 'react-toolbox';
 import Input from 'react-toolbox/lib/input';
 import Link from 'react-toolbox/lib/link';
 import Dialog from 'react-toolbox/lib/dialog';
 import styles from '../public/styles.css';
-import axios from 'axios';
-
 import Form from './components/form.jsx';
-const {generatePassphrase, handleSender, handleMessage, handleDate, postMsg, handleEncrypt, handleClose, decryptMsg, handleDecrypt, handleDialogInput, createNewPassphrase, handleSnackbarTimeout} = require('./utils.js');
+
+const { generatePassphrase, handleSender, handleMessage, handleDate, postMsg, handleEncrypt, handleClose, decryptMsg, handleDecrypt, handleDialogInput, createNewPassphrase, handleSnackbarTimeout } = require('./utils.js');
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       sender: '',
@@ -21,41 +20,50 @@ class App extends Component {
       passphrase: '',
       active: false,
       showDialog: '',
-      showSnackbar: false
-    }
-    
+      showSnackbar: false,
+    };
+    this.handleClose = handleClose.bind(this);
+    this.decryptMsg = decryptMsg.bind(this);
+    this.handleSender = handleSender.bind(this);
+    this.handleMessage = handleMessage.bind(this);
+    this.handleDate = handleDate.bind(this);
+    this.handleEncrypt = handleEncrypt.bind(this);
+    this.handleDecrypt = handleDecrypt.bind(this);
+    this.createNewPassphrase = createNewPassphrase.bind(this);
+    this.handleDialogInput = handleDialogInput.bind(this);
+    this.handleSnackbarTimeout = handleSnackbarTimeout.bind(this);
   }
 
   componentDidMount() {
     this.setState({
-      passphrase: generatePassphrase()
+      passphrase: generatePassphrase(),
     });
   }
 
-  render(){
-    //actions for Dialog buttons
+  render() {
+    // actions for Dialog buttons
     const actions = [
-      { label: 'CLOSE', onClick: handleClose.bind(this) },
-      { label: 'DECRYPT', onClick: decryptMsg.bind(this) },
+      { label: 'CLOSE', onClick: this.handleClose },
+      { label: 'DECRYPT', onClick: this.decryptMsg },
     ];
 
     return (
       <div className="container">
         <Form
-          sender={this.state.sender} 
-          handleSender={handleSender.bind(this)}
+          sender={this.state.sender}
+          handleSender={this.handleSender}
           unencrypted={this.state.unencrypted}
-          handleMessage={handleMessage.bind(this)}
+          handleMessage={this.handleMessage}
           date={this.state.date}
-          handleDate={handleDate.bind(this)}
-          handleEncrypt={handleEncrypt.bind(this)}
-          decryptMsg={handleDecrypt.bind(this)} 
+          handleDate={this.handleDate}
+          handleEncrypt={this.handleEncrypt}
+          decryptMsg={this.handleDecrypt}
         />
 
         <div className="btn-container">
 
           <Link href="#" label={`Your passphrase - ${this.state.passphrase}`} />
-          <Link onClick={createNewPassphrase.bind(this)} href="#" label="Generate new Passphrase" />
+          <Link onClick={this.createNewPassphrase} href="#" label="Generate new Passphrase" />
 
         </div>
 
@@ -64,14 +72,14 @@ class App extends Component {
           active={this.state.active}
           title="De/Encrypt"
         >
-          <Input 
-            type='text' 
-            label='Message' 
-            name='message' 
-            multiline 
+          <Input
+            type="text"
+            label="Message"
+            name="message"
+            multiline
             value={this.state.showDialog}
-            onChange={handleDialogInput.bind(this)} 
-            />
+            onChange={this.handleDialogInput}
+          />
 
         </Dialog>
 
@@ -80,12 +88,12 @@ class App extends Component {
             active={this.state.showSnackbar}
             label="You're message is missing something. Please fill out the form   entirely."
             timeout={3500}
-            onTimeout={handleSnackbarTimeout.bind(this)}
+            onTimeout={this.handleSnackbarTimeout}
           />
         </section>
 
       </div>
-    )
+    );
   }
 }
 
